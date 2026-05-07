cabff6028dd99085321323408775deb33846d098
Harman Soni <harman.codes.dev@gmail.com>
2026-05-07 14:06:51 +0530

    Server js file update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,13 @@ import app from "./app.js";
 import env from "./config/env.js";
 import { connectMongo } from "./config/mongo.js";
 
-await connectMongo();
+// Try to connect to MongoDB, but don't fail if it can't connect
+if (process.env.MONGODB_URI) {
+	connectMongo().catch((err) => {
+		console.warn("MongoDB connection failed:", err.message);
+		console.warn("Continuing without MongoDB...");
+	});
+}
 
 const server = app.listen(env.port, () => {
 	console.log(`Backend running on port ${env.port}`);
