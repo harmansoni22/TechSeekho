8f293d15926d6199d1af7e4f227ed2702a1653bb
Harman Soni <harman.codes.dev@gmail.com>
2026-05-07 12:08:49 +0530

    mongo js upload

--- backend/src/config/mongo.js
+++ backend/src/config/mongo.js
@@ -5,7 +5,8 @@ export const connectMongo = async () => {
 		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("MongoDB Connected Successfully");
 	} catch (err) {
-		console.error(err);
-		process.exit(1);
+		console.error("MongoDB connection error:", err.message);
+		// Don't exit - allow server to run without MongoDB
+		throw err;
 	}
 };
