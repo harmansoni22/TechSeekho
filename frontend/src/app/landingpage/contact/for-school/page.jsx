ab31a5917800397aa5550224681f2f1511f96e88
Harman Soni <harman.codes.dev@gmail.com>
2026-05-08 17:27:53 +0530

    Adding We are hiring , and contant page for schools

---
+++ frontend/src/app/landingpage/contact/for-school/page.jsx
@@ -0,0 +1,5 @@
+const ContactForSchool = () => {
+    return <h1>Contact for School route</h1>
+}
+
+export default ContactForSchool;
---
+++ frontend/src/app/landingpage/contact/we-are-hiring/page.jsx
@@ -0,0 +1,5 @@
+const WeAreHiring = () => {
+    return <h1>We are Actually Hiring!</h1>
+}
+
+export default WeAreHiring;
