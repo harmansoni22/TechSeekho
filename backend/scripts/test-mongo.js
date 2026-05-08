3cfd94a8091e951dcfa1dd31da7656b60b800606
Harman Soni <harman.codes.dev@gmail.com>
2026-05-08 07:10:01 +0530

    Update test-mongo.js

--- backend/scripts/test-mongo.js
+++ backend/scripts/test-mongo.js
@@ -1,9 +1,8 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://techseekho:harmansoni@techseekho-dev-cluster.zzthcwa.mongodb.net/?appName=techseekho-dev-cluster";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
